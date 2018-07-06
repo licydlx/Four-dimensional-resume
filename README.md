9adedd8e8f8638d8b04ee5803cd08b394f0acc76
ydlx <lichong536@163.com>
2018-07-06 19:06:53 +0800

    四维

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Four-dimensional-resume
 四维
+
+
+
+1.html 		 二维
+2.css        三维
+4.javascript 四维
---
+++ server.js
@@ -0,0 +1,15 @@
+﻿var express = require('express');
+var path = require('path')
+var app = express();
+
+app.use(express.static('src'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.resolve(__dirname,  './src/index.html'));
+})
+
+const port = 8000;
+
+var server = app.listen(port, function () {
+  console.log("访问地址为 http://localhost:" + port)
+});
